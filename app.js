2accefbf02a792791c1ce9a51da11884aa264f60
PBI <phillip.biermann@nextlytics.com>
2018-12-08 17:51:59 +0100

    Update score

---
+++ app.js
@@ -0,0 +1,47 @@
+// // *************************************
+// // Chart.js
+// // *************************************
+// // Global Settings
+// Chart.defaults.global.defaultFontColor = "rgba(43,43,43,1)";
+// Chart.defaults.global.defaultFontFamily = "'ApexNew-Medium', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
+// Chart.defaults.global.defaultFontSize = 12;
+// Chart.defaults.global.defaultFontStyle = "normal";
+// Chart.defaults.global.maintainAspectRatio = false;
+// // Disable click on legend
+// Chart.defaults.global.legend.onClick = (e) => e.stopPropagation();
+
+// var ctx = document.getElementById("lean-chart")
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ["19.11.", "20.11.", "21.11", "22.11.", "23.11", "24.11", "25.11" ],
+//         datasets: [{
+//             label: 'Phil Weight',
+//             data: [93.5, 92.6, , 91.4, , XX ],
+//             backgroundColor: '#3E93BC'
+//         },
+//         {
+//             label: '',
+//             data: [],
+//             backgroundColor: '#076897'
+//         },
+        
+//         {
+//             label: 'Line 1',
+            
+//             type: 'line',
+//             backgroundColor: '#076897'     
+
+            
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         }
+//     }
+// });
